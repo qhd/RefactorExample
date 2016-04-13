290666a4d07117b53e3961bd0ebbb741889dc35f
qiuhaodong <qiuhaodong2006@126.com>
2016-04-13 17:59:14 +0800

    把amountFor()移到Rental类去

--- Customer.java
+++ Customer.java
@@ -55,23 +55,7 @@ public class Customer {
         return result;
     }
 
-    private float amountFor(Rental aRental) {
-        float result = 0;
-        switch (aRental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (aRental.getDaysRented() > 2)
-                    result += (aRental.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += aRental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (aRental.getDaysRented() > 3)
-                    result += (aRental.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return result;
+    private double amountFor(Rental aRental) {
+        return aRental.getCharge();
     }
 }
